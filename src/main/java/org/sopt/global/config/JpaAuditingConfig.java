87a11d5d33c6bbfb5faf6c39e9ee235274856281
haerxeong <haeryeong04@naver.com>
2026-05-11 16:12:51 +0900

    refactor: config 위치를 global 아래로 수정

--- src/main/java/org/sopt/global/config/JpaAuditingConfig.java
+++ src/main/java/org/sopt/global/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package org.sopt.config;
+package org.sopt.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
