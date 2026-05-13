abe0d1bdc5f3e870b1588b1b61c7c5317edf2fe6
jy000n <jayoonchang@naver.com>
2026-05-14 01:30:30 +0900

    docs: 공지사항 조회 API Swagger 명세 개선

--- src/main/java/org/sopt/controller/NoticeController.java
+++ src/main/java/org/sopt/controller/NoticeController.java
@@ -1,6 +1,8 @@
 package org.sopt.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.sopt.dto.response.NoticeResponse;
@@ -26,6 +28,20 @@ public class NoticeController {
             summary = "공지사항 전체 조회",
             description = "전체 공지사항 목록을 조회합니다."
     )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "공지사항 전체 조회 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "공지사항을 찾을 수 없음"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버 내부 오류"
+            )
+    })
     @GetMapping
     public ResponseEntity<ApiResponseBody<List<NoticeResponse>, Void>> getAllNotices(){
         return ResponseEntity.status(SuccessCode.OK.getStatus())
