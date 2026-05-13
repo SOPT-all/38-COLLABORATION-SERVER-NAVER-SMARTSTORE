5e44cdd49e69eef7dc2658522aca85a5801280bb
jy000n <jayoonchang@naver.com>
2026-05-14 01:19:20 +0900

    refactor: Notice API 제네릭 응답 타입 구체화

--- src/main/java/org/sopt/controller/NoticeController.java
+++ src/main/java/org/sopt/controller/NoticeController.java
@@ -3,6 +3,7 @@ package org.sopt.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.sopt.dto.response.NoticeResponse;
 import org.sopt.service.NoticeService;
 import org.sopt.global.response.ApiResponseBody;
 import org.sopt.global.response.SuccessCode;
@@ -10,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "Notice", description = "공지사항 API")
 @RestController
@@ -24,7 +27,7 @@ public class NoticeController {
             description = "전체 공지사항 목록을 조회합니다."
     )
     @GetMapping
-    public ResponseEntity<ApiResponseBody<?, Void>> getAllNotices(){
+    public ResponseEntity<ApiResponseBody<List<NoticeResponse>, Void>> getAllNotices(){
         return ResponseEntity.status(SuccessCode.OK.getStatus())
                 .body(
                         ApiResponseBody.ok(
