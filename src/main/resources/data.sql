4a9815674c66ffda6359c71a07e8dfe03e1fceec
jy000n <jayoonchang@naver.com>
2026-05-13 16:45:41 +0900

    chore: application.yml 및 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO notice (notice_type, title, created_at)
+SELECT '시스템', '정산 일일 리포트 발송 일시 중단 안내', '2026-04-02 14:30:00'
+WHERE NOT EXISTS (
+    SELECT 1 FROM notice WHERE title = '정산 일일 리포트 발송 일시 중단 안내'
+);
