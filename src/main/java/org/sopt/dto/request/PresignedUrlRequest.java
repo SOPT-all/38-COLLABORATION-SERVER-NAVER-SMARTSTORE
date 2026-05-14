a59ac6f778c713de08848be3cb9a15926fdec827
jy000n <jayoonchang@naver.com>
2026-05-15 02:24:21 +0900

    chore: PresignedUrlRequest DTO validation 메시지 추가

--- src/main/java/org/sopt/dto/request/PresignedUrlRequest.java
+++ src/main/java/org/sopt/dto/request/PresignedUrlRequest.java
@@ -3,6 +3,10 @@ package org.sopt.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record PresignedUrlRequest (
-  @NotBlank String fileName,
-  @NotBlank String ContentType
+
+  @NotBlank(message = "파일명은 필수입니다.")
+  String fileName,
+
+  @NotBlank(message = "content_type은 필수입니다.")
+  String contentType
 ) {}
