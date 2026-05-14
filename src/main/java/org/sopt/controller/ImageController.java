4a988a91929bb9e922e496326b4268d6790a8573
jy000n <jayoonchang@naver.com>
2026-05-15 04:24:28 +0900

    docs: Swagger 문구 수정

--- src/main/java/org/sopt/controller/ImageController.java
+++ src/main/java/org/sopt/controller/ImageController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Image", description = "이미지 관련 API")
+@Tag(name = "Image", description = "이미지 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/images")
