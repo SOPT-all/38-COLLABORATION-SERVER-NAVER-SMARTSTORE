ca6130d90c85431bb2db74e3385aba728b8e769b
haerxeong <haeryeong04@naver.com>
2026-05-15 03:59:17 +0900

    style: 들여쓰기 수정

--- src/main/java/org/sopt/global/response/SuccessCode.java
+++ src/main/java/org/sopt/global/response/SuccessCode.java
@@ -6,18 +6,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum SuccessCode {
 
-    OK(HttpStatus.OK, "요청이 성공했습니다."),
-    CREATED(HttpStatus.CREATED, "리소스가 생성되었습니다."),
+  OK(HttpStatus.OK, "요청이 성공했습니다."),
+  CREATED(HttpStatus.CREATED, "리소스가 생성되었습니다."),
 
-    // =================== PRODUCT ===================
-    PRODUCT_FOUND(HttpStatus.OK, "상품 조회에 성공했습니다."),
-    PRODUCT_CREATED(HttpStatus.CREATED, "상품 등록에 성공했습니다.");
+  // =================== PRODUCT ===================
+  PRODUCT_FOUND(HttpStatus.OK, "상품 조회에 성공했습니다."),
+  PRODUCT_CREATED(HttpStatus.CREATED, "상품 등록에 성공했습니다.");
 
-    private final HttpStatus status;
-    private final String message;
+  private final HttpStatus status;
+  private final String message;
 
-    SuccessCode(HttpStatus status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+  SuccessCode(HttpStatus status, String message) {
+    this.status = status;
+      this.message = message;
+  }
 }
