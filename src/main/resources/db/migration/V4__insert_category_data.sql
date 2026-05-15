916581ecaaff121ecdd850496def89c3d87ab6a4
haerxeong <haeryeong04@naver.com>
2026-05-15 21:23:56 +0900

    feat: 카테고리 데이터 추가

---
+++ src/main/resources/db/migration/V4__insert_category_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO category (name, created_at, updated_at)
+VALUES
+    ('FURNITURE_INTERIOR', NOW(), NOW()),
+    ('BOOK', NOW(), NOW()),
+    ('DIGITAL_APPLIANCE', NOW(), NOW()),
+    ('HEALTH', NOW(), NOW()),
+    ('SPORTS_LEISURE', NOW(), NOW()),
+    ('FOOD', NOW(), NOW()),
+    ('LEISURE_CONVENIENCE', NOW(), NOW()),
+    ('BABY_PARENTING', NOW(), NOW()),
+    ('FASHION_CLOTHES', NOW(), NOW()),
+    ('FASHION_ACCESSORIES', NOW(), NOW()),
+    ('COSMETIC_BEAUTY', NOW(), NOW());
