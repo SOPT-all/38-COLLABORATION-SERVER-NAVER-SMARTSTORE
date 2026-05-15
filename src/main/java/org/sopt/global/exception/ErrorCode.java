1505abbccce4575350876a2b026cf13c7264ef00
jy000n <jayoonchang@naver.com>
2026-05-15 16:32:17 +0900

    feat: 파일 크기 초과 에러 코드 추가 (IMG_005)

--- src/main/java/org/sopt/global/exception/ErrorCode.java
+++ src/main/java/org/sopt/global/exception/ErrorCode.java
@@ -34,6 +34,7 @@ public enum ErrorCode {
   REPRESENTATIVE_IMAGE_REQUIRED(HttpStatus.BAD_REQUEST, "IMG_002", "대표 이미지가 필요합니다."),
   MULTIPLE_REPRESENTATIVE_IMAGES(HttpStatus.BAD_REQUEST, "IMG_003", "대표 이미지는 하나만 허용됩니다."),
   UNSUPPORTED_MIME_TYPE(HttpStatus.BAD_REQUEST, "IMG_004", "지원하지 않는 이미지 형식입니다."),
+  FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "IMG_005", "파일 크기는 5MB를 초과할 수 없습니다."),
 
   // 404 NOT FOUND
   IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "IMG_001", "존재하지 않는 이미지입니다."),
