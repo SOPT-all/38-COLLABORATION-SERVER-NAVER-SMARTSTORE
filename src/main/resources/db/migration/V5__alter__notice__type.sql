ea6c1932a02418446705f7c0a1ec28e63edb033e
jy000n <jayoonchang@naver.com>
2026-05-15 22:41:41 +0900

    chore: notice_type 컬럼 ENUM 타입으로 변경 (V5)

---
+++ src/main/resources/db/migration/V5__alter__notice__type.sql
@@ -0,0 +1,14 @@
+ALTER TABLE notice
+MODIFY COLUMN notice_type ENUM(
+    'GENERAL',
+    'DELIVERY',
+    'SAFE_TRADE',
+    'SELLING_TIP',
+    'D_COMMERCE',
+    'HAZARD_INFO',
+    'MANUAL'
+);
+
+UPDATE notice
+SET notice_type = 'SYSTEM'
+WHERE notice_type = '시스템';
